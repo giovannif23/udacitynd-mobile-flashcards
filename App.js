fd1c52579ca684231e4d656f51d91694c6387a8d
Giovanni Filippi <giovannif23@gmail.com>
2017-11-13 20:39:26 -0500

    Trying to get TabNavigator working

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar, Platform, TouchableOpacity } from 'react-native'
+import { TabNavigator } from 'react-navigation'
+import { primary, white, black } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+function Home () {
+  return (
+    <View>
+      <Text style={styles.text}>HOME!</Text>
+    </View>
+  )
+}
+
+function Contact () {
+  return (
+    <View>
+      <Text style={styles.text}>Contact!</Text>
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  Home: {
+    screen: Home,
+  },
+  Contact: {
+    screen: Contact
+  },
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Tabs />
       </View>
     );
   }
@@ -16,8 +42,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: black,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: white,
+    fontSize: 40
+  }
 });
---
+++ utils/colors.js
@@ -0,0 +1,5 @@
+export const primary = '#468189'
+export const white = '#fafafa'
+export const black = '#031926'
+export const grey = '#e3f2fd'
+export const red = '#db5461'
